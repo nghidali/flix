093cbaaecde12bc50e34493208e08449cdf89797
Natalie Ghidali <nghidali@fb.com>
2018-06-28 15:26:56 -0700

    added tab bar and README

---
+++ flix/View Controllers/MoviesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoviesViewController.h
+//  flix
+//
+//  Created by Natalie Ghidali on 6/28/18.
+//  Copyright © 2018 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesViewController : UIViewController
+
+@end
