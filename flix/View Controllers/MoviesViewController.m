efe274488c07423c3ac659a7266c1b45347df699
Natalie Ghidali <nghidali@fb.com>
2018-06-28 17:05:25 -0700

    basic collection view

--- flix/View Controllers/MoviesViewController.m
+++ flix/View Controllers/MoviesViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "MoviesViewController.h"
+#import "MovieCell.h"
+#import "MovieCollectionViewCell.h"
+#import "UIImageView+AFNetworking.h"
 
-@interface MoviesViewController ()
+@interface MoviesViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+@property (strong, nonatomic) NSMutableArray<NSDictionary*> *movies;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
 
@@ -17,11 +22,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    
+    [self fetchMovies];
+    
+    //resize collection view
+    UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout*) self.collectionView.collectionViewLayout;
+    CGFloat postersPerLine = 2;
+    CGFloat itemWidth = self.collectionView.frame.size.width / postersPerLine;
+    CGFloat itemHeight = 1.5 * itemWidth;
+    layout.itemSize = CGSizeMake(itemWidth,itemHeight);
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)fetchMovies {
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        // This will run when the network request returns
+        if (error) {
+            NSLog(@"%@", error);
+        } else if (data) {
+            NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            
+            self.movies = dataDictionary[@"results"];
+            // Update UI in main thread
+            dispatch_async(dispatch_get_main_queue(), ^{
+
+            });
+        }
+        
+    }];
+    [task resume];
 }
 
 /*
@@ -33,5 +73,26 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    
+    MovieCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionViewCell" forIndexPath:indexPath];
+    
+    NSDictionary *movie = self.movies[indexPath.item];
+    
+    NSString *posterPathString = movie[@"poster_path"];
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSURL *url = [NSURL URLWithString:[baseURLString stringByAppendingString:posterPathString]];
+    cell.posterView.image = nil;
+    [cell.posterView setImageWithURL:url];
+
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section { 
+    return self.movies.count;
+}
+
+
 
 @end
---
+++ flix/Views/MovieCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.m
+//  flix
+//
+//  Created by Natalie Ghidali on 6/28/18.
+//  Copyright © 2018 Emerson Malca. All rights reserved.
+//
+
+#import "MovieCollectionViewCell.h"
+
+@implementation MovieCollectionViewCell
+
+@end
